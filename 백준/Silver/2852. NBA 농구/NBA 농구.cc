5049e26aab1a7c122163b1cfa9f45dc7dcc8ae6f
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-12-21 08:55:53 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-int n, A, B, aSum, bSum;
-int team;
+int n, A, B, aSum, bSum, team;
 string t, tmp;
+
 string makeTimeStamp(int value)
 {
     string m = "00" + to_string(value / 60);
     string s = "00" + to_string(value % 60);
+
     return m.substr(m.size() - 2, 2) + ":" + s.substr(s.size() - 2, 2);
 }
 
-int changeToInt(string a)
+int changeToInt(string s)
 {
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+    return atoi(s.substr(0, 2).c_str()) * 60 + atoi(s.substr(3, 2).c_str());
 }
 
 void solve(int &sum, string s)
 {
-    sum += (changeToInt(s) - changeToInt(tmp));
+    sum += changeToInt(s) - changeToInt(tmp);
 }
 
 int main()
@@ -32,22 +33,15 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> team >> t;
-
         if (A > B)
-        {
             solve(aSum, t);
-        }
         else if (A < B)
-        {
             solve(bSum, t);
-        }
         team == 1 ? A++ : B++;
         tmp = t;
     }
     if (A > B)
-    {
         solve(aSum, "48:00");
-    }
     else if (A < B)
     {
         solve(bSum, "48:00");
@@ -55,5 +49,6 @@ int main()
 
     cout << makeTimeStamp(aSum) << "\n";
     cout << makeTimeStamp(bSum);
+
     return 0;
 }
