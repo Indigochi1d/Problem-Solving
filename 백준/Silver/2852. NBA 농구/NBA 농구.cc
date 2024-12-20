d6780b12df205fc249ad85f1d900dbd778082232
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-12-21 08:40:48 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, A, B, aSum, bSum;
+int team;
+string t, tmp;
+string makeTimeStamp(int value)
+{
+    string m = "00" + to_string(value / 60);
+    string s = "00" + to_string(value % 60);
+    return m.substr(m.size() - 2, 2) + ":" + s.substr(s.size() - 2, 2);
+}
+
+int changeToInt(string a)
+{
+    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+}
+
+void solve(int &sum, string s)
+{
+    sum += (changeToInt(s) - changeToInt(tmp));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> team >> t;
+
+        if (A > B)
+        {
+            solve(aSum, t);
+        }
+        else if (A < B)
+        {
+            solve(bSum, t);
+        }
+        team == 1 ? A++ : B++;
+        tmp = t;
+    }
+    if (A > B)
+    {
+        solve(aSum, "48:00");
+    }
+    else if (A < B)
+    {
+        solve(bSum, "48:00");
+    }
+
+    cout << makeTimeStamp(aSum) << "\n";
+    cout << makeTimeStamp(bSum);
+    return 0;
+}
