89a6b0d13922569f264bff2385e33fead335c62a
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-12-01 00:02:20 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+    int n;
+    cin >> n;
+
+    vector<int> d(n + 1, 0);
+
+    for (int i = 2; i <= n; i++) {
+        d[i] = d[i - 1] + 1;  
+
+        if (i % 2 == 0)       
+            d[i] = min(d[i], d[i / 2] + 1);
+
+        if (i % 3 == 0)        
+            d[i] = min(d[i], d[i / 3] + 1);
+    }
+
+    cout << d[n] << "\n";
+    return 0;
+}
